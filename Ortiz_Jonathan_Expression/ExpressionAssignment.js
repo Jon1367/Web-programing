ca04e7c5517b318de292bef1b5e0977dc154e608
Jon1367 <jonathano1367@yahoo.com>
2014-01-18 12:46:12 -0800

    console.log and variables
    Created new variables  to hold NameLength and total of numbers.
    console.log the user input for A B C.

--- Ortiz_Jonathan_Expression/ExpressionAssignment.js
+++ Ortiz_Jonathan_Expression/ExpressionAssignment.js
@@ -6,13 +6,33 @@
 
 // Creating Assigment again after deleting with new Repositorie
 
-var First = prompt("Enter your First name:");
-var Last = prompt("Enter your Last name:");
+
+// Personal
+
+var First = prompt("What is your First name:");
+var Last = prompt("What is your Last name:");
 
 var FullName = First +" " +  Last;
 
 var UserNumbers = [];
 
+var NameLength = First.length + Last.length;
+
+console.log("Hello " + FullName + " it's nice to meet you let me show you my math skills.My name  is Math B")
+
+console.log( FullName + " you have a total of " + NameLength + " letters in your name." )
+
 UserNumbers[0] = prompt(" Choose a number for A:")
 UserNumbers[1] = prompt(" Choose a number for B:")
 UserNumbers[3] = prompt(" Choose a number for C:")
+
+console.log("A = " + UserNumbers[0]);
+console.log("B = " + UserNumbers[1]);
+console.log("C = " + UserNumbers[2]);
+
+
+var total = parseInt(UserNumbers[0]) + parseInt(UserNumbers[1]) + parseInt(UserNumbers[2]);
+
+
+var average = total / 3;
+
