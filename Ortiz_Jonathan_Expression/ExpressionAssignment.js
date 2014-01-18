4c207dbe073817b6528b843e8b0af0555181d449
Jon1367 <jonathano1367@yahoo.com>
2014-01-18 12:18:29 -0800

    Created Varables
    Created var to hold user first and last name. Then store them in
    FullName. Created an array to do some math with user numbers a , b ,c

--- Ortiz_Jonathan_Expression/ExpressionAssignment.js
+++ Ortiz_Jonathan_Expression/ExpressionAssignment.js
@@ -6,3 +6,13 @@
 
 // Creating Assigment again after deleting with new Repositorie
 
+var First = prompt("Enter your First name:");
+var Last = prompt("Enter your Last name:");
+
+var FullName = First +" " +  Last;
+
+var UserNumbers = [];
+
+UserNumbers[0] = prompt(" Choose a number for A:")
+UserNumbers[1] = prompt(" Choose a number for B:")
+UserNumbers[3] = prompt(" Choose a number for C:")
