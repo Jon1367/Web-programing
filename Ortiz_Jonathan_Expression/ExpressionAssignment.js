b8d3f95a28ca17f3504b8331a88af986fc76f49d
Jon1367 <jonathano1367@yahoo.com>
2014-01-18 15:05:32 -0800

    Created math  variables to solve problems.To show how the computer does math.  Also created more dialogue.

--- Ortiz_Jonathan_Expression/ExpressionAssignment.js
+++ Ortiz_Jonathan_Expression/ExpressionAssignment.js
@@ -9,8 +9,8 @@
 
 // Personal
 
-var First = prompt("What is your First name:");
-var Last = prompt("What is your Last name:");
+var First = prompt("(>^ ^)> { What is your First name:}");
+var Last = prompt("(>^ ^)>  { What is your Last name: }");
 
 var FullName = First +" " +  Last;
 
@@ -18,21 +18,29 @@ var UserNumbers = [];
 
 var NameLength = First.length + Last.length;
 
-console.log("Hello " + FullName + " it's nice to meet you. My name is Math bot let me show you what i can do.")
+prompt(" (>^ ^)> { Hello " + FullName + " it's nice to meet you. My name is Math bot let me show you what i can do.}");
 
 console.log( FullName + " you have a total of " + NameLength + " letters in your name." )
 
-UserNumbers[0] = prompt(" Choose a number for A:")
-UserNumbers[1] = prompt(" Choose a number for B:")
-UserNumbers[2] = prompt(" Choose a number for C:")
-
+UserNumbers[0] = prompt(" (>^ ^)> { Choose a number for A:")
+UserNumbers[1] = prompt(" (>^ ^)> { Choose a number for B:")
+UserNumbers[2] = prompt(" <(^ ^)> { Choose a number for C:")
+UserNumbers[3] = prompt(" (^ ^) [Last one]  { Choose a number for D:")
+console.log("(>^ ^)> { Your choice are: ");
 console.log("A = " + UserNumbers[0]);
 console.log("B = " + UserNumbers[1]);
 console.log("C = " + UserNumbers[2]);
+console.log("D = " + UserNumbers[3]);
 
 
-var total = parseInt(UserNumbers[0]) + parseInt(UserNumbers[1]) + parseInt(UserNumbers[2]);
+var total = parseInt(UserNumbers[0]) + parseInt(UserNumbers[1]) + parseInt(UserNumbers[2]) + parseInt(UserNumbers[3]);
+var fractionAddTop = parseInt(UserNumbers[0] + parseInt(UserNumbers[2]))
+var fractionAddBottom = parseInt(UserNumbers[1] + parseInt(UserNumbers[4]));
 
-var average = total / 3;
+var average = total / 4;
 
-console.log("")
+console.log(" A + B + C + D = " + total + " The average is: " + average);
+console.log("Level 2! A/B + C/D = " + fractionAddTop + " / " + fractionAddBottom);
+console.log(" Are you mesmerized");
+
+
