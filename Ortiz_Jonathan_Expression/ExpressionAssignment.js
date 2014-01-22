33b2dc2a6c9e04750e83eedb2f834ac4a112dfa9
Jon1367 <jonathano1367@yahoo.com>
2014-01-21 21:14:02 -0800

    Add comment
    to make code more readable

--- Ortiz_Jonathan_Expression/ExpressionAssignment.js
+++ Ortiz_Jonathan_Expression/ExpressionAssignment.js
@@ -10,42 +10,45 @@
 // Personal
 
 
-// 
+// Creating String and minipulating strings.
 var First = prompt(" (>^ ^)> { What is your First name:}");
 var Last = prompt("(>^ ^)>  { What is your Last name: }");
 var FullName = First +" " +  Last;
 var UserNumbers = [];
 var NameLength = First.length + Last.length;
 
+// Display Name of User and (>^^>
 prompt(" (>^ ^)> Hello " + FullName + " it's nice to meet you. My name is Math bot let me show you what i can do.");
 
 console.log( FullName + " you have a total of " + NameLength + " letters in your name." )
 
+// Array to store users input
 UserNumbers[0] = prompt(" (>^ ^)> { Choose a number for A:");
 UserNumbers[1] = prompt(" (>^ ^)> { Choose a number for B:");
 UserNumbers[2] = prompt(" <(^ ^)> { Choose a number for C:");
-UserNumbers[3] = prompt(" (^ ^) [Last one]  { Choose a number for D:");
+UserNumbers[3] = prompt(" (^ ^)[Last one]{ Choose a number for D:");
 
+// Display User's Input
 console.log("(>^ ^)> { Your choices are: ");
 console.log("A = " + UserNumbers[0]);
 console.log("B = " + UserNumbers[1]);
 console.log("C = " + UserNumbers[2]);
 console.log("D = " + UserNumbers[3]);
 
-
+// Total of all UserInput
 var total = parseFloat(UserNumbers[0]) + parseFloat(UserNumbers[1]) + parseFloat(UserNumbers[2]) + parseFloat(UserNumbers[3]);
-
+var average = total / 4;
+// Adding fraction
 var fractionAddTop = parseFloat(UserNumbers[0]) + parseFloat(UserNumbers[2]);
 var fractionAddBottom = parseFloat(UserNumbers[1]) + parseFloat(UserNumbers[3]);
 var totalfraction = parseFloat(fractionAddTop) / parseFloat(fractionAddBottom);
 
+// Muiltplying Fraction
 var fractionMuiltTop = parseFloat(UserNumbers[1]) * parseFloat(UserNumbers[2]);
 var fractionMuiltBottom = parseFloat(UserNumbers[0]) * parseFloat(UserNumbers[3]);
 var muiltFraction = parseFloat(fractionMuiltTop) / parseFloat(fractionMuiltBottom);
 
-
-var average = total / 4;
-
+// Display solution
 console.log(" A + B + C + D = " + total + " The average is: " + average);
 console.log(" A/B + C/D = " + fractionAddTop + " / " + fractionAddBottom + " = " + totalfraction);
 console.log(" A/B * C/D = " + fractionMuiltTop + " / " + fractionMuiltBottom + " = " + muiltFraction);
@@ -53,30 +56,33 @@ console.log(" A/B * C/D = " + fractionMuiltTop + " / " + fractionMuiltBottom + "
 // Industry
 prompt(" (>^ ^)> Math bot can also keep track out Company Spending:");
 
+// Ask User for Inputs
 var income = prompt("(>^^)> How much does your company make per month?");
 var NunmberOfEmpolyees = prompt("(>^^)> How many empolyees do you have: ");
 var empolyeesSalary = prompt("(>^^)> How much do they get paid an hour:")
 var Bills = prompt(("(>^^)> How much is Bills each month: "));
-                                              // 8 hours it average number of work days.
-var YearSalary = parseFloat(empolyeesSalary) * 8 * 248; // 248 number of days in a year without weekends or holidays
-var AllEmpolyeesSalary = parseFloat(YearSalary) * NunmberOfEmpolyees;
-var YearIncome = parseFloat(income) * 12;
-var YearBills = parseFloat(Bills) * 12;
 
+// Made varaibles for year                   - 8 hours it average number of work days.
+var YearSalary = parseInt(empolyeesSalary) * 8 * 248; // 248 number of days in a year without weekends or holidays
+var AllEmpolyeesSalary = parseInt(YearSalary) * parseInt(NunmberOfEmpolyees);
+var YearIncome = parseInt(income) * 12;
+var YearBills = parseInt(Bills) * 12;
 
-var TotalAfterBills = parseFloat(YearIncome) - parseFloat(YearBills);
-var TotalAfterEmpolyees = parseFloat(TotalAfterBills) - parseFloat(AllEmpolyeesSalary);
+// Amount left after paying bills and empolyees
+var TotalAfterBills = parseInt(YearIncome) - parseInt(YearBills);
+var TotalAfterEmpolyees = parseInt(TotalAfterBills) - parseInt(AllEmpolyeesSalary);
 
 
 
 prompt( "(>^^)> cacualting... ");
 
+// Display info
 console.log(FullName + " your company makes " + YearIncome + " a year and spends:");
-console.log( "Bills = " + YearBills);
+console.log( "Year Bills = $" + YearBills);
 console.log(" After Bills : $" + TotalAfterBills);
-console.log(" A single Empolyee's salary is $" + YearSalary + " Base on 8 hours a week and work five days of work  " +
+console.log(" A single Empolyee's salary per year  is $" + YearSalary + " Base on 8 hours a week and work five days of work  " +
     " All Empolyees's year salaries is $" +AllEmpolyeesSalary );
-console.log( FullName + " your company as a remainder of $" + TotalAfterEmpolyees);
+console.log( FullName + " your company has a remainder of $" + TotalAfterEmpolyees);
 
 // Wackly
 
