9bc6e54159da49e80901604906179ae396f91e43
Jon1367 <jonathano1367@yahoo.com>
2014-01-22 22:38:12 -0800

    Created comments and more if statement for game
    Created a block of code to determine the winner

--- Ortiiz_Jonathan_Conditionals /main.js
+++ Ortiiz_Jonathan_Conditionals /main.js
@@ -1,25 +1,33 @@
 // Assignment Conditionals
 
-// Created
+// Created Quit variable for while condition
 var Quit = true;
 
 // Created varibale for User's Name
 var playerName = prompt(" Enter your name:");
-// Created a random choice for computer;
+
+// Display friendly message to user
 prompt(" (>^^)> -  Hello " + playerName + " Lets play Rock, Paper , sisscors" + "\n"+" Hit enter to countiune...)");
 
+// Created loop to keep game looping
 while(Quit){
 
+// Created a random choice for computer
 var computerChoice = Math.floor(Math.random()* (3 - 1 + 1) + 1);
 
+// Created variables for Rock , Paper , Sisscors
 var Rock = 1;
 var Paper =2;
 var Sisscors = 3;
 
-
+// Display options to user
 var UserChoice = prompt(" Here are your choices: " + "\n" + "1) Rock" + "\n" + "2) Paper" + "\n" + "3) Sisscors"
     + "4) Quit");
 
+    // if statements to exit loop
+    if( parseInt(UserChoice) == 4 ){ Quit = false}
+
+    // Display User choice
     if( parseInt(UserChoice) == Rock){
         prompt(playerName +" choices Rock");
     }else if(parseInt(UserChoice) === Paper){
@@ -28,6 +36,7 @@ var UserChoice = prompt(" Here are your choices: " + "\n" + "1) Rock" + "\n" + "
         prompt(playerName + " choices Sisscors")
     }else{ Quit = true}
 
+    // Display Computer choice
     if( computerChoice == Rock){
         prompt(" Computer choices Rock");
     }else if(computerChoice === Paper){
@@ -36,6 +45,16 @@ var UserChoice = prompt(" Here are your choices: " + "\n" + "1) Rock" + "\n" + "
         prompt("Computer choices Sisscors")
     }else{ Quit = true}
 
-    if( parseInt(UserChoice) == 4 ){ Quit = false}
+    // Determine Tie
+    if(parseInt(UserChoice) == Rock && computerChoice == Rock){
+        prompt(" Rocks Tie");
+    }else if(parseInt(UserChoice) == Paper && computerChoice == Paper){
+        prompt(" Papers Tie")
+    }else if( parseInt(UserChoice) == Sisscors && computerChoice == Sisscors){
+        prompt(" Sisscors Tie")
+    }
+
+    // Determine Winner
+    if( parseInt(UserChoice))
 
 }
