e2dba9684a07626dc06d47cf565a50fae89e8aa6
Jon1367 <jonathano1367@yahoo.com>
2014-01-22 21:31:52 -0800

    Created var for player's name and inputs
    ALso made computer choice Random; Also included math bot (>^^)>

--- Ortiiz_Jonathan_Conditionals /main.js
+++ Ortiiz_Jonathan_Conditionals /main.js
@@ -1,9 +1,23 @@
 // Assignment Conditionals
 
-var playerName = prompt(" Enter your name:");
+var Quit = 0;
 
+while(UserChoice != Quit){
+
+
+// Created varibale for User's Name
+var playerName = prompt(" Enter your name:");
+// Created a random choice for computer;
 var computerChoice = Math.floor(Math.random()* (3 - 1 + 1) + 1);
 
-prompt(" (>^^)> -  Hello" + playerName + " Lets play Rock, Paper , sisscors ")
+var Rock = 1;
+var Paper =2;
 
-prompt(" Here are the Rules: " + "\n" + );
+prompt(" (>^^)> -  Hello" + playerName + " Lets play Rock, Paper , sisscors ");
+
+var UserChoice = prompt(" Here are your choices: " + "\n" + "1) Rock" + "\n" + "2) Paper" + "\n" + "3) Sisscors"
+    + "4) Quit");
+
+    if( UserChoice == )
+
+}
