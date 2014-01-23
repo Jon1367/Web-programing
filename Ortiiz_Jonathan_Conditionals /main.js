31646d604be1de306926ae41cafec495dff5c714
Jon1367 <jonathano1367@yahoo.com>
2014-01-22 22:21:05 -0800

    if statement and fix
    created an if statement  for both the computer and users choices. Aslo
    debug the repeating prompt on line 9;

--- Ortiiz_Jonathan_Conditionals /main.js
+++ Ortiiz_Jonathan_Conditionals /main.js
@@ -1,23 +1,41 @@
 // Assignment Conditionals
 
-var Quit = 0;
-
-while(UserChoice != Quit){
-
+// Created
+var Quit = true;
 
 // Created varibale for User's Name
 var playerName = prompt(" Enter your name:");
 // Created a random choice for computer;
+prompt(" (>^^)> -  Hello " + playerName + " Lets play Rock, Paper , sisscors" + "\n"+" Hit enter to countiune...)");
+
+while(Quit){
+
 var computerChoice = Math.floor(Math.random()* (3 - 1 + 1) + 1);
 
 var Rock = 1;
 var Paper =2;
+var Sisscors = 3;
 
-prompt(" (>^^)> -  Hello" + playerName + " Lets play Rock, Paper , sisscors ");
 
 var UserChoice = prompt(" Here are your choices: " + "\n" + "1) Rock" + "\n" + "2) Paper" + "\n" + "3) Sisscors"
     + "4) Quit");
 
-    if( UserChoice == )
+    if( parseInt(UserChoice) == Rock){
+        prompt(playerName +" choices Rock");
+    }else if(parseInt(UserChoice) === Paper){
+        prompt(playerName + " choices Paper")
+    }else if(parseInt(UserChoice) == Sisscors){
+        prompt(playerName + " choices Sisscors")
+    }else{ Quit = true}
+
+    if( computerChoice == Rock){
+        prompt(" Computer choices Rock");
+    }else if(computerChoice === Paper){
+        prompt("Computer choices Paper")
+    }else if(computerChoice == Sisscors){
+        prompt("Computer choices Sisscors")
+    }else{ Quit = true}
+
+    if( parseInt(UserChoice) == 4 ){ Quit = false}
 
 }
