74893412a0044f8c9d45ce4487af36ba7c5f077c
Jon1367 <jonathano1367@yahoo.com>
2014-01-27 22:50:48 -0800

    forgot to stop and commit Created few variables and while loop
    Created an area for potion and potion price plan on created 4 more
    arrays for weapon and armor. Created if statements the one on line 14
    is for user choice on what item to buy.

--- Ortiiz_Jonathan_Conditionals /Industry.js
+++ Ortiiz_Jonathan_Conditionals /Industry.js
@@ -4,8 +4,31 @@
 
 var instore = true;
 
+var potion = [" Health Potion","Empty Bottle","Oil" ];
+var potionprice = [20,5,10];
+
+var useritems = [];
+
 while(instore){
 
+var UserChoice  = prompt("(>^^)> Welcome to the Hyrlue here you can trade and purchuse items:" + "\n"
+    + "1) Potions " + "\n" +
+      "2) Weapons" + "\n" +
+      "3) Armor " + "\n" +
+      "4) Trade" + "\n" +
+      "5) Quit")
+
+    if(instore == 5){ instore = false}
+
+    if(UserChoice == 1){
+
+   var buypotions = prompt("(>^^)> These are the Potions i have avaiable: " + " \n" +
+   "1)" + potion[0] + "cost = " + potionprice[0] + "\n" +
+   "2)" + potion[1] + "cost = " + potionprice[1] + "\n" +
+   "3)" + potion[2] + "cost = " + potionprice[2] + "\n");
+
+
+    }
 
 
 }
