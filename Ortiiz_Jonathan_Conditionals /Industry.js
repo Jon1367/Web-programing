23cd242884c30686a9d402cecb2ba9f3889877c8
Jon1367 <jonathano1367@yahoo.com>
2014-01-27 23:24:44 -0800

    created variables and if statements
    Created if statement for choices of items to buy then nest another if
    statement to check witch item they want to buy and hot much of it.

--- Ortiiz_Jonathan_Conditionals /Industry.js
+++ Ortiiz_Jonathan_Conditionals /Industry.js
@@ -2,12 +2,21 @@
  * Created by Jonathan Ortiz on 1/27/14.
  */
 
-var instore = true;
-
+// Array for potion and price
 var potion = [" Health Potion","Empty Bottle","Oil" ];
 var potionprice = [20,5,10];
 
-var useritems = [];
+// Array for Weapons
+var Weapons = ["Sword","Hammer","Bow"];
+var Weaponsprice = [20, 50, 30];
+
+// Array for Armor
+var Amors = ["Leather Armor","Iron Armor", "Dragon Armor"]
+var useritems = [100, 200, 300];
+
+var userMoney = 5000;
+
+var instore = true;
 
 while(instore){
 
@@ -16,19 +25,27 @@ var UserChoice  = prompt("(>^^)> Welcome to the Hyrlue here you can trade and pu
       "2) Weapons" + "\n" +
       "3) Armor " + "\n" +
       "4) Trade" + "\n" +
-      "5) Quit")
+      "5) Quit");
+
+   if(UserChoice == 5){ instore = false}
+
+   if(UserChoice == 1){
+   var buypotions = prompt("(>^^)> These are the Potions i have avaiable: " + " \n" +
+    "a) " + potion[0] + " cost = $" + potionprice[0] + "\n" +
+    "b) " + potion[1] + " cost = $" + potionprice[1] + "\n" +
+    "c) " + potion[2] + " cost = $" + potionprice[2] + "\n");
+        if( buypotions == 'a' || buypotions == 'A'){
+            var amount = prompt( potion[0] + " cost " + potionprice[0] + " How many do you want?");
+        }
+
+    }else if(UserChoice == 2){
+    prompt("(>^^)> Here are the weapons i have:" + "\n" +
+    "a) " + Weapons[0] + " cost = $" + Weaponsprice[0] + "\n" +
+    "b) " + Weapons[1] + " cost = $" + Weaponsprice[1] + "\n" +
+    "c) " + Weapons[2] + " cost = $" + Weaponsprice[2] + "\n" +)
+
+   }
 
     if(instore == 5){ instore = false}
-
-    if(UserChoice == 1){
-
-   var buypotions = prompt("(>^^)> These are the Potions i have avaiable: " + " \n" +
-   "1)" + potion[0] + "cost = " + potionprice[0] + "\n" +
-   "2)" + potion[1] + "cost = " + potionprice[1] + "\n" +
-   "3)" + potion[2] + "cost = " + potionprice[2] + "\n");
-
-
-    }
-
 
 }
