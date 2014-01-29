7f2d2fe212740d32bc5688852505a6f96a254e8f
Jon1367 <jonathano1367@yahoo.com>
2014-01-28 16:42:27 -0800

    Created Sell Weapons and check if program checks for errors
    It took some debugging for it to finally work right . you can know sell
    all items, need to put if statement to tell when the user out of items
    to sell

--- Ortiiz_Jonathan_Conditionals /Industry.js
+++ Ortiiz_Jonathan_Conditionals /Industry.js
@@ -10,28 +10,22 @@ var potionprice = [20,5,10];
 var Weapons = ["Sword","Hammer","Bow"];
 var Weaponsprice = [20, 50, 30];
 
-// Array for Armor
-var Armor = ["Leather Armor","Iron Armor", "Dragon Armor"]
-var ArmorPrice = [100, 200, 300];
 
 // User's money
 var userMoney = 5000;
 
-//Amount Healthpotion
-var AmountOfHealthPotion = 0;
-var AmountOfPotion = 0;
+//Amount Potion
 var TotalHealthPotion = 0;
-
-// Amount of  Empty bottle
-var AmountOfEmptyBottle = 0;
-var AmountOfpotion = 0;
 var TotalEmptyBottle = 0;
-
-
 var TotalOil = 0;
+
+// Amount Weapons
+var totalSword = 0;
+var totalHammer = 0;
+var totalBow = 0;
+
 // while condition
 var instore = true;
-
 
 // loops as long as instore is true
 while(instore){
@@ -43,32 +37,36 @@ var UserChoice  = prompt("(>^^)> Welcome to the Hyrlue here you can trade and pu
       "3)Trade" + "\n" +
       "4) Quit" + "\n" );
 
-    //prompt(useritems[0]);
     // Quit loop
-   if(UserChoice == 5){ instore = false}
+   if(UserChoice == 4){ instore = false}
 
-    // if statement to check what was the user choice
+   // if statement to check what was the user choice 1,2,3 or 4
    if(UserChoice == 1){
-    // Created a variable to hold which potion would you want to buy
+   // Created a variable to hold which item would user want to buy
    var buypotions = prompt("(>^^)> These are the Potions i have avaiable: " + " \n" +
     "a) " + potion[0] + " cost = $" + potionprice[0] + "\n" +
     "b) " + potion[1] + " cost = $" + potionprice[1] + "\n" +
     "c) " + potion[2] + " cost = $" + potionprice[2] + "\n");
+
+        // Check which item user choice then ask he want to buy muiltiple of item
         if( buypotions == 'a' || buypotions == 'A'){
             var amount = prompt( potion[0] + " cost " + potionprice[0] + " How many do you want?");
+            // pay for the amount they want and substract it from user's money
             userMoney -= potionprice[0] * parseInt(amount);
+            // Store amount of item user
             TotalHealthPotion += parseInt(amount);
-
         }else if( buypotions == 'b' || buypotions == 'B'){
             var amount2 = prompt(potion[1] +" cost " + potionprice[1] + "How many do you want?");
-            userMoney -= potionprice[1] + parseInt(amount2);
+            userMoney -= potionprice[1] * parseInt(amount2);
             TotalEmptyBottle += parseInt(amount2);
 
         }else if( buypotions == 'c' || buypotions == 'C'){
             var amount3 = prompt(potion[2] + " cost " + potionprice[2] + "How many do you want?" );
-            userMoney -= potionprice[0] + parseInt(amount3);
+            userMoney -= potionprice[0] * parseInt(amount3);
             TotalOil += parseInt(amount3)
 
+        }else{
+            prompt("Invaild Input!");
         }
 
     }else if(UserChoice == 2){
@@ -76,21 +74,23 @@ var UserChoice  = prompt("(>^^)> Welcome to the Hyrlue here you can trade and pu
     "a) " + Weapons[0] + " cost = $" + Weaponsprice[0] + "\n" +
     "b) " + Weapons[1] + " cost = $" + Weaponsprice[1] + "\n" +
     "c) " + Weapons[2] + " cost = $" + Weaponsprice[2] + "\n");
-       if( buypotions == 'a' || buypotions == 'A'){
+       if( buyweapons == 'a' || buypotions == 'A'){
            var want = prompt( Weapons[0] + " cost " + Weaponsprice[0] + " How many do you want?");
-           userMoney -= Weapons[0] * parseInt(want);
-           TotalHealthPotion += parseInt(want);
+           userMoney -= Weaponsprice[0] * parseInt(want);
+           totalSword += parseInt(want);
 
-       }else if( buypotions == 'b' || buypotions == 'B'){
+       }else if( buyweapons == 'b' || buypotions == 'B'){
            var want2 = prompt(Weapons[1] +" cost " + Weaponsprice[1] + "How many do you want?");
-           userMoney -= potionprice[1] + parseInt(want2);
-           TotalEmptyBottle += parseInt(want2);
+           userMoney -= Weaponsprice[1] * parseInt(want2);
+           totalHammer += parseInt(want2);
 
-       }else if( buypotions == 'c' || buypotions == 'C'){
+       }else if( buyweapons == 'c' || buypotions == 'C'){
            var want3 = prompt(Weapons[2] + " cost " + Weaponsprice[2] + "How many do you want?" );
-           userMoney -= potionprice[0] + parseInt(want3);
-           TotalOil += parseInt(want3);
+           userMoney -= Weaponsprice[2] * parseInt(want3);
+            totalBow += parseInt(want3);
 
+       }else{
+           prompt("Invaild Input")
        }
 
    }else if( UserChoice == 3){
@@ -99,27 +99,74 @@ var UserChoice  = prompt("(>^^)> Welcome to the Hyrlue here you can trade and pu
 
        while(selling){
            var sell = prompt("(>^^)> What would you like to sell" + "\n" +
+           playerName + " has $" + userMoney + "\n" +
            "a) Potion" + "\n" +
            "b) Weapons" + "\n" +
-           "c) Quit selling");
-                if( sell == 'a' || sell == 'b'){
-                    var SellPotion = prompt(" Which potion would you like to sell");
+           "f) Finish selling");
+                if( sell == 'a' || sell == 'A'){
+                    var SellPotion = prompt(" Which potion would you like to sell " + "\n" +
+                    "j) " + potion[0] + " sells for $" + potionprice[0] / 2 + "\n" +
+                    "k) " + potion[1] + " sells for $ " + potionprice[1] / 2 + "\n" +
+                    "l)" +  potion[2] + " sells for $ " + potionprice[2] / 2 );
+
+                        if( SellPotion == 'j' || SellPotion == 'J'){
+                          var SellHealth = prompt(" How  Health potion would you like to sell:");
+                            TotalHealthPotion -= parseInt(SellHealth);
+                            userMoney += potionprice[0] / 2 * parseInt(SellHealth);
+                        }else if( SellPotion == 'k' || SellPotion == 'K'){
+                            var SellBottle = prompt(" How  Empty Bottle would you like to sell:");
+                            TotalEmptyBottle -= parseInt(SellBottle);
+                            userMoney += potionprice[1] / 2 * parseInt(SellBottle);
+                        }else if( SellPotion == 'l' || SellPotion == 'L'){
+                            var SellOIl = prompt(" How  Oil would you like to sell:");
+                            TotalOil -= parseInt(SellOIl);
+                            userMoney += potionprice[2] / 2 * parseInt(SellOIl);
+                        }else{
+                            prompt("Error");
+                        }
                 }else if( sell == "b"  || sell == 'B'){
 
-                }else if( sell == "C" || sell == "c"){
+                        var SellWeapon = prompt(" Which potion would you like to sell " + "\n" +
+                            "j) " + Weapons[0] + " sells for $" + Weaponsprice[0] / 2 + "\n" +
+                            "k) " + Weapons[1] + " sells for $ " + Weaponsprice[1] / 2 + "\n" +
+                            "l) " +  Weapons[2] + " sells for $ " + Weaponsprice[2] / 2 );
 
+                        if( SellWeapon == 'j' || SellPotion == 'J'){
+                            var SellSword = prompt(" How  many swords would you like to sell:");
+                            totalSword -= parseInt(SellSword);
+                            userMoney += potionprice[0] / 2 * parseInt(SellSword);
+                        }else if( SellWeapon == 'k' || SellPotion == 'K'){
+                            var SellHammer = prompt(" How many Hammers would you like to sell:");
+                            totalHammer -= parseInt(SellHammer);
+                            userMoney += potionprice[1] / 2 * parseInt(SellHammer);
+                        }else if( SellWeapon == 'l' || SellPotion == 'L'){
+                            var SellBow = prompt(" How many Bows would you like to sell:");
+                            totalBow -= parseInt(SellBow);
+                            userMoney += potionprice[2] / 2 * parseInt(SellBow);
+                        }else{
+                            prompt("Error");
+                        }
+
+                }else if( sell == "f" || sell == "f"){
+                    selling = false;
                 }
 
        }
 
+   }else{
+       prompt("Error 101:");
    }
 
 
     if(instore == 5){ instore = false}
 
-}
+
 
 prompt(" You have " + userMoney + "\n" +
       "Total Health" + TotalHealthPotion + "\n" +
-       "Total Empty Bottle" + TotalEmptyBottle + "\n" +
-       "Total Oil" + TotalOil + "\n");
+      "Total Empty Bottle:" + TotalEmptyBottle + "\n" +
+      "Total Oil:" + TotalOil + "\n" +
+      "Total Sword:" + totalSword + "\n" +
+      "Total Hammer:" + totalHammer + "\n" +
+      "Total Bow : " + totalBow + "\n" )
+}
---
+++ Ortiiz_Jonathan_Conditionals /wacky.js
@@ -0,0 +1,23 @@
+
+var PlayingDice = true;
+
+while(PlayingDice){
+var UserDice = prompt("(>^^)> Hello " + playerName + " Lets play some dice:" + "\n" +
+    "1) Roll 6 sided dice" + "\n" +
+    "2) Roll 12 sided dice " + "\n" +
+    "3) Roll 36 sided dic " + "\n" +
+    "4) Quit");
+
+    if(UserDice == 4){ PlayingDice = false}
+    
+    if( UserDice == 1){
+
+    }else if( UserDice == 2){
+
+    }else if( UserDice == 3){
+
+    }else{
+
+    }
+
+}
