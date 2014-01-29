24202c5178e16f2e3734502a0053ee66cc29a193
Jon1367 <jonathano1367@yahoo.com>
2014-01-28 19:43:56 -0800

    Created computer and User random dice
    left the variables outside the loop so i can test program. Creating if
    statements to determine winner and keep score

--- Ortiiz_Jonathan_Conditionals /wacky.js
+++ Ortiiz_Jonathan_Conditionals /wacky.js
@@ -1,16 +1,36 @@
 
 var PlayingDice = true;
 
+// variables for computer's dice
+var computerSixDice = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+var computer12dice = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+var computer36dice = Math.floor(Math.random() * (36 - 1 + 1) + 1);
+
+var playerSixDice = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+var player12dice = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+var player36dice = Math.floor(Math.random() * (36 - 1 + 1) + 1);
+
+
+
+
 while(PlayingDice){
 var UserDice = prompt("(>^^)> Hello " + playerName + " Lets play some dice:" + "\n" +
-    "1) Roll 6 sided dice" + "\n" +
-    "2) Roll 12 sided dice " + "\n" +
-    "3) Roll 36 sided dic " + "\n" +
+    "1) Roll two 6 sided dice" + "\n" +
+    "2) Roll two 12 sided dice " + "\n" +
+    "3) Roll two 36 sided dic " + "\n" +
     "4) Quit");
 
     if(UserDice == 4){ PlayingDice = false}
-    
+
     if( UserDice == 1){
+
+        if(playerSixDice > computerSixDice){
+            prompt( "You Win! rolled a " + playerSixDice + " computer rolled a " + computerSixDice)
+        }else if(playerSixDice < computerSixDice){
+            prompt("You loss! Computer rolled a" + computerSixDice+  "  you rolled a " + playerSixDice)
+        }else{
+            prompt("Tie");
+        }
 
     }else if( UserDice == 2){
 
