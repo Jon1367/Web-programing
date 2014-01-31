3e97254a35f38a2e3906dee6a0f64f5dc848178a
Jon1367 <jonathano1367@yahoo.com>
2014-01-30 20:33:17 -0800

    Creating function i can reuse getting to understand function better. Also in creating option for each function

--- Ortiz_Jonathan_Function/Personal.js
+++ Ortiz_Jonathan_Function/Personal.js
@@ -1,8 +1,7 @@
 /**
- * Created by Spike1367 on 1/29/14.
+ * Created by Jonathan Ortiz on 1/29/14.
  */
 
-// f
 
 
 //Created variables
@@ -15,34 +14,75 @@ var UsersNumber = [ 'a', 'b' ,'c' ];
 // created a function to return username when called
 function name( fullname){
 
-
     fullname = first + " " +  last;
     return fullname;
 }
 
 // Display user's full  name and enter choice for a b c
-prompt(" Hello " + name() + " (>^^)>Math bot learn to use Function to solve problems: " +"\n");
+prompt(" Hello " + name() + " (>^^)> Math bot learn to use Function to solve problems: " +"\n");
          UsersNumber[0] =  prompt(" Enter a number for a:");
          UsersNumber[1] =  prompt(" Enter a number for b:");
          UsersNumber[2] =  prompt(" Enter a number for c:");
 
-prompt(" (>^^)>  What would you like to do with a , b c:" + "\n" +
+var game = true;
+while(game){
+
+var choice = prompt(" (>^^)>  What would you like to do with a , b c:" + "\n" +
         " 1)Math " + "\n" +
         " 2)Math Game " + "\n" +
-        " 3)Quit")
+        " 3)Quit");
+
+        if( choice == 1 ){
+            prompt(" Here are the your inputs"  + "\n" +
+               " a + b + c  = " + math()  +"\n" +
+               " a  / b / c  = " + math(this.dvide))
+        }else if(choice == 2){
+
+        }else if(choice == 3){
+
+             quit();
+        }
+
+}
+
+function quit(){
+
+    var quiter = prompt("Are you sure you want to quit? Y or N:");
+                if( quiter == 'Y' || quiter == 'y'){
+                    game = false;
+                    //return game;
+                }else if( quiter == 'N' || quiter == 'n'){
+                    game = true;
+                   // return game;
+                }else( prompt("Error"));
+
+}
+
+function math(total,Exponents,divide){
+
+    total = parseInt(UsersNumber[0]) + parseInt(UsersNumber[1]) + parseInt(UsersNumber[2]);
+
+    var mathchoice = prompt(" (>^^)>  What kind of math would you like to do with your numbers?" +"\n" +
+                             "a) Add my numbers " + "\n" +
+                             "b) Divide my numbers" + "\n" +
+                             "c) Muiltiply my numbers ");
+
+        if( mathchoice == 'a' || mathchoice == 'A'){
+            prompt(" (>^^)> here are a + b  + c total: " + "\n"+
+                       UsersNumber[0] + " + " + UsersNumber[1] + " + " + UsersNumber[2] + " = " + total)
+        }else if( mathchoice == 'b' || mathchoice == 'B'){
+            prompt(" (>^^)> here are a / b / c divided each other")
+        }
+
+    Exponents = parseInt(UsersNumber[0]) + parseInt(UsersNumber[1]) + parseInt(UsersNumber[2]);
+
+     divide = parseInt(UsersNumber[0]) / parseInt(UsersNumber[1])  %  parseInt(UsersNumber[2]);
 
 
 
-var UsrChoice = prompt(" (>^^)> Choose the next function you would like to use numbers:");
-
-function math(total){
-
-     //otal = parseInt(UsersNumber[0]) + parseInt(UsersNumber[1]) + parseInt(UsersNumber[2]);
-
-    return total;
+    return total , divide , Exponents;
 }
 
 
-prompt(" Total = " + Add());
 
-function
+
