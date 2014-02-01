c6d2d93fa4af91c2f2b665e11b20f34cffa93694
Jon1367 <jonathano1367@yahoo.com>
2014-02-01 13:30:39 -0800

    Debuging
    trying to call array to add new info, debuting code

--- Ortiz_Jonathan_Function/Industry.js
+++ Ortiz_Jonathan_Function/Industry.js
@@ -8,13 +8,13 @@ var MathbotMessenger = true;
 // loop for program
 while(MathbotMessenger){
     // Get user dessecion
-    var userInput = prompt("(>^^)> Welcome to Mathbot Messenger a 10x better version of yahoo Messenger." +"\n" +
-                           "1) Inbox " + "\n" +
-                           "2) Sent " + "\n" +
+    var userInput = prompt("(>^^)> Welcome to Mathbot Messenger." +"\n" +
+                           "1) Info " + "\n" +
+                           "2) Add Contact " + "\n" +
                            "3) Contacts " + "\n" +
                            "4) Quit");
     //if statement to quit loop
-    if(userInput == 4){MathbotMessenger = false;}
+    if(userInput == 4){quit()}
 
     // if statement to call functions
     if(userInput == 1){
@@ -22,10 +22,33 @@ while(MathbotMessenger){
     }else if(userInput == 2){
 
     }else if(userInput == 3){
-
+        prompt(contacts());
     }else(prompt("Error"));
 }
 
-function inbox(emails,name){
+function contacts(list,name,email,phoneNumber){
 
+    list = [];
+    name = [];
+    email = [];
+    phoneNumber = [];
+    list[0] = "Jonathan Ortiz";
+    list[1] = "Corey Davenport";
+    list[2] = "Donald  Page";
+
+   for(i = 0; i < list.length;i++){
+       console.log(list[i] + "\n");
+   }
+
+}
+function AddContact(name,email,phoneNumber){
+
+    
+}
+function Sent(){
+
+}function quit(){
+
+    MathbotMessenger = false;
+    return MathbotMessenger;
 }
--- Ortiz_Jonathan_Function/Personal.js
+++ Ortiz_Jonathan_Function/Personal.js
@@ -6,7 +6,7 @@
 
 //Created variables
 var first = prompt(" What is your first name:");
-var last =prompt(" What is your last name")
+var last =prompt(" What is your last name");
 
 // Array to store users numbers
 var UsersNumber = [ 'a', 'b' ,'c' ];
@@ -27,20 +27,22 @@ prompt(" Hello " + name() + " (>^^)> Math bot learn to use Function to solve pro
 //Created true condtion for infinite loop
 var game = true;
 while(game){
-// Created a variable choice to score user choide
+// Created a variable choice to score user choice
 var choice = prompt(" (>^^)>  What would you like to do with a , b c:" + "\n" +
         " 1)Math " + "\n" +
         " 2)Change numbers " + "\n" +
-        " 3)Quit");
+         "3)Theory of probability " + "\n"  +
+        " 4)Quit");
         // if statment to call functions
         if( choice == 1 ){
             math();
         }else if(choice == 2){
             changeNumber();
         }
-        if(choice == 3){
-
-             quit();
+        else if(choice == 3){
+            theory();
+        }else if( choice == 4){
+            quit();
         }
 
 }
@@ -57,12 +59,12 @@ function quit(){
                 }else( prompt("Error"));
 }
 // Do math function
-function math(total,divide,Muilt){
+function math(){
 
     // math variables
-    total = parseInt(UsersNumber[0]) + parseInt(UsersNumber[1]) + parseInt(UsersNumber[2]);
-    Muilt = parseInt(UsersNumber[0]) + parseInt(UsersNumber[1]) + parseInt(UsersNumber[2]);
-    divide = parseInt(UsersNumber[0]) / parseInt(UsersNumber[1])  %  parseInt(UsersNumber[2]);
+    var total = parseInt(UsersNumber[0]) + parseInt(UsersNumber[1]) + parseInt(UsersNumber[2]);
+    var Muilt = parseInt(UsersNumber[0]) + parseInt(UsersNumber[1]) + parseInt(UsersNumber[2]);
+    var divide = parseInt(UsersNumber[0]) / parseInt(UsersNumber[1])  %  parseInt(UsersNumber[2]);
     // give user a choice
     var mathchoice = prompt(" (>^^)>  What kind of math would you like to do with your numbers?" +"\n" +
                              "a) Add my numbers " + "\n" +
@@ -81,10 +83,10 @@ function math(total,divide,Muilt){
         }else{
             prompt("Error invailed input");
         }
-    // keep loop going learned after i called function it would break loop
-    game = true;
-    // return true
-    return game;
+//    // keep loop going learned after i called function it would break loop
+//    game = true;
+//    // return true
+//    return game;
 }
 // Created a function to change the number
 function changeNumber(){
@@ -94,6 +96,64 @@ function changeNumber(){
     UsersNumber[2] =  prompt(" Enter a number for c:");
 
 return true;
+}
+function theory(){
+
+    var guessright = 0;
+    var guesswrong = 0;
+
+    var   theoryguessright = 0;
+    var   theoryguesswrong = 0;
+
+    var testTheory = true;
+
+    while(testTheory){
+
+    var random = Math.floor(math() * (3 - 1 + 1) + 1);
+
+
+    var user = prompt("(>^^)>  Theory of probability is you are more likely to guess the right choice " + "\n" +
+                            "if you make  a choice then change it." +"\n" +
+                                "results:" + "\n" +
+                                  "guess  right = " + guessright + " guess wrong = " + guesswrong + "\n" +
+                                   " Theory right = " + theoryguessright + " wrong = " + theoryguesswrong + "\n" +
+                                    "j) guess " + "\n" +
+                                    "k) theory" + "\n" +
+                                    "l) quit");
+
+                             if(user == 'j' || user == 'J'){
+                                  var guess  = prompt( "1) (>^^)> 2) (>^^)> 3) (>^^)>" + "\n"+
+                                 " (>^^)> Which one is the real Math bot:");
+
+                                 if(guess = random){
+                                     prompt("You guess right my guess was:" + random);
+                                     guessright++;
+                                 }else if(guess != random){
+                                     prompt("You guess wrong my guess was:" + random);
+                                     guesswrong++;
+                                 }
+
+
+                             }else if(user == 'k' || user == 'K' ){
+                                var guess2 = prompt("1) (>^^)> 2) (>^^)> 3) (>^^)>" + "\n"+
+                                     " This time make a chose then before you enter a number change your choice " + "\n" +
+                                     " (>^^)> Which of your numbers did i chose:");
+
+                                 if(guess2 = random){
+                                     prompt("You guess right" + " guess was :" + random);
+                                     theoryguessright++;
+                                 }else if(guess2 != random){
+                                     prompt("You guess wrong" + " guess was :" + random);
+                                     theoryguesswrong++;
+                                 }
+
+
+                             }else if( user == 'l' || user == 'L'){
+                                 testTheory = false;
+                             }
+
+
+    }
 }
 
 
