9474a7e0988ded2539bc502cd87f593a8ae3c014
Jon1367 <jonathano1367@yahoo.com>
2014-01-31 17:14:49 -0800

    cleaning up code and debuging
    Created industry program

---
+++ Ortiz_Jonathan_Function/Industry.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Jonathan Ortiz on 1/31/14.
+ */
+
+var MathbotMessenger = true;
+
+while(MathbotMessenger){
+
+    if(userInput == 4){
+        MathbotMessenger = false;
+    }else if(userInput){
+
+    }else if(userInput){
+
+    }
+}
--- Ortiz_Jonathan_Function/Personal.js
+++ Ortiz_Jonathan_Function/Personal.js
@@ -24,27 +24,27 @@ prompt(" Hello " + name() + " (>^^)> Math bot learn to use Function to solve pro
          UsersNumber[1] =  prompt(" Enter a number for b:");
          UsersNumber[2] =  prompt(" Enter a number for c:");
 
+//Created true condtion for infinite loop
 var game = true;
 while(game){
-
+// Created a variable choice to score user choide
 var choice = prompt(" (>^^)>  What would you like to do with a , b c:" + "\n" +
         " 1)Math " + "\n" +
-        " 2)Math Game " + "\n" +
+        " 2)Change numbers " + "\n" +
         " 3)Quit");
-
+        // if statment to call functions
         if( choice == 1 ){
-            prompt(" Here are the your inputs"  + "\n" +
-               " a + b + c  = " + math()  +"\n" +
-               " a  / b / c  = " + math(this.dvide))
+            math();
         }else if(choice == 2){
-
-        }else if(choice == 3){
+            changeNumber();
+        }
+        if(choice == 3){
 
              quit();
         }
 
 }
-
+// Quit function to ask if user is done
 function quit(){
 
     var quiter = prompt("Are you sure you want to quit? Y or N:");
@@ -55,32 +55,45 @@ function quit(){
                     game = true;
                    // return game;
                 }else( prompt("Error"));
-
 }
+// Do math function
+function math(total,divide,Muilt){
 
-function math(total,Exponents,divide){
-
+    // math variables
     total = parseInt(UsersNumber[0]) + parseInt(UsersNumber[1]) + parseInt(UsersNumber[2]);
-
+    Muilt = parseInt(UsersNumber[0]) + parseInt(UsersNumber[1]) + parseInt(UsersNumber[2]);
+    divide = parseInt(UsersNumber[0]) / parseInt(UsersNumber[1])  %  parseInt(UsersNumber[2]);
+    // give user a choice
     var mathchoice = prompt(" (>^^)>  What kind of math would you like to do with your numbers?" +"\n" +
                              "a) Add my numbers " + "\n" +
                              "b) Divide my numbers" + "\n" +
                              "c) Muiltiply my numbers ");
-
+        // if statement to do math with user numbers
         if( mathchoice == 'a' || mathchoice == 'A'){
             prompt(" (>^^)> here are a + b  + c total: " + "\n"+
-                       UsersNumber[0] + " + " + UsersNumber[1] + " + " + UsersNumber[2] + " = " + total)
+                       UsersNumber[0] + " + " + UsersNumber[1] + " + " + UsersNumber[2] + " = " + total);
         }else if( mathchoice == 'b' || mathchoice == 'B'){
-            prompt(" (>^^)> here are a / b / c divided each other")
+            prompt(" (>^^)> here are a / b / c divided each other " +"\n" +
+                    UsersNumber[0] + " / " + UsersNumber[1] + " / "  + " / "+ UsersNumber[2] + " = " + divide);
+        }else if( mathchoice == 'c' || mathchoice == 'C'){
+            prompt(" (>^^)> here are  a * b * c" + "\n" +
+                     UsersNumber[0] +" x " + UsersNumber[1] + " x " + UsersNumber[2] + " = " + Muilt );
+        }else{
+            prompt("Error invailed input");
         }
+    // keep loop going learned after i called function it would break loop
+    game = true;
+    // return true
+    return game;
+}
+// Created a function to change the number
+function changeNumber(){
 
-    Exponents = parseInt(UsersNumber[0]) + parseInt(UsersNumber[1]) + parseInt(UsersNumber[2]);
+    UsersNumber[0] =  prompt(" Enter a number for a:");
+    UsersNumber[1] =  prompt(" Enter a number for b:");
+    UsersNumber[2] =  prompt(" Enter a number for c:");
 
-     divide = parseInt(UsersNumber[0]) / parseInt(UsersNumber[1])  %  parseInt(UsersNumber[2]);
-
-
-
-    return total , divide , Exponents;
+return true;
 }
 
 
