d889f31ace9075e23294023f460252393ff8477f
Jon1367 <jonathano1367@yahoo.com>
2014-01-31 17:33:35 -0800

    created variables and if statements
    Line.17 - if statement to break loop
    line.19 - if , else if and if statement for function

--- Ortiz_Jonathan_Function/Industry.js
+++ Ortiz_Jonathan_Function/Industry.js
@@ -2,15 +2,30 @@
  * Created by Jonathan Ortiz on 1/31/14.
  */
 
+    // Loop condition
 var MathbotMessenger = true;
 
+// loop for program
 while(MathbotMessenger){
+    // Get user dessecion
+    var userInput = prompt("(>^^)> Welcome to Mathbot Messenger a 10x better version of yahoo Messenger." +"\n" +
+                           "1) Inbox " + "\n" +
+                           "2) Sent " + "\n" +
+                           "3) Contacts " + "\n" +
+                           "4) Quit");
+    //if statement to quit loop
+    if(userInput == 4){MathbotMessenger = false;}
 
-    if(userInput == 4){
-        MathbotMessenger = false;
-    }else if(userInput){
+    // if statement to call functions
+    if(userInput == 1){
 
-    }else if(userInput){
+    }else if(userInput == 2){
 
-    }
+    }else if(userInput == 3){
+
+    }else(prompt("Error"));
+}
+
+function inbox(emails,name){
+
 }
